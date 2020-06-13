ecfc2c6cd3b826c35d3cc302cbf277edcc15660a
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-06-14 00:24:36 +0300

    Updated styles

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,18 @@
 from django.views.generic import TemplateView, ListView
 
+from blog.models import Post
 from .models import Employee
 
 
 class HomePage(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["latest_posts"] = Post.objects.all()[:4]
+
+        return context
 
 
 class OurTeamView(ListView):
